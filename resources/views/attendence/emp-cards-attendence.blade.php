79b1e39f8ba377934ddec6927f5e3b0f5f9d8e01
Muhammad Abubakar <m3abubakar@gmail.com>
2024-03-01 15:59:51 +0500

    changing

--- resources/views/attendence/emp-cards-attendence.blade.php
+++ resources/views/attendence/emp-cards-attendence.blade.php
@@ -63,9 +63,7 @@
                                     <button class="reblateBtn w-75"><span style="width: 15px; height: 15px; margin-right: 5px;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
                                       </svg></span> Search</button>
-                                    <button class="reblateBtn w-75"><span style="width: 15px; height: 15px; margin-right: 5px;"><svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
-                                      </svg></span> Add New</button>
+
                                 </div>
                             </div>
                         </form>
